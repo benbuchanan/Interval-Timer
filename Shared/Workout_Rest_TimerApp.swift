87158fff906f942ab24806d16e2df9a916f1bf81
Ben Buchanan <blbuchan@ncsu.edu>
2022-01-07 12:19:01 -0500

    Initial Commit

---
+++ Shared/Workout_Rest_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_Rest_TimerApp.swift
+//  Shared
+//
+//  Created by Ben Buchanan on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Workout_Rest_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
