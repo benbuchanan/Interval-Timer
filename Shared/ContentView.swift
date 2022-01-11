6493fbf92361b6bc3b4f8c940238c2043c7954b5
benbuchanan <benbuchanan11@yahoo.com>
2022-01-11 18:27:52 -0500

    First commit to Interval Timer

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TimerView(duration: 30)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct TimerView: View {
+    
+    @State var duration: Int
+    @State var fill: CGFloat = 0
+        
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                // Title and settings button
+                HStack {
+                    Text("Set Timer").font(.largeTitle).frame(maxWidth: .infinity, alignment: .leading).padding(20)
+                    Button("-Settings-") {
+                        print("settings click")
+                    }.padding(20)
+                }
+                Spacer()
+            }
+                
+                // Circle and Timer
+                ZStack {
+                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .bottomLeading, endPoint: .topTrailing)).frame(width: 150, height: 150)
+                    Text("\(self.duration)").font(.system(size: 56)).foregroundColor(.white)
+                        .onReceive(timer) { time in
+                            if self.duration > 0 {
+                                self.duration -= 1
+                            }
+                        }
+                        .onDisappear() {
+                            self.timer.upstream.connect().cancel()
+                        }
+                }
+                    .onAppear() {
+                        self.fill = 1.0
+                    }
+                Spacer()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+struct Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
