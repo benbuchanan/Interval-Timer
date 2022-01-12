651572e1dadf979026985a29675ee04239f7a531
benbuchanan <benbuchanan11@yahoo.com>
2022-01-11 19:19:51 -0500

    added stop, pause/resume, and repeat buttons.  Made center timer circle and text bigger.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -34,23 +34,56 @@ struct TimerView: View {
                 Spacer()
             }
                 
-                // Circle and Timer
-                ZStack {
-                    Circle().fill(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .bottomLeading, endPoint: .topTrailing)).frame(width: 150, height: 150)
-                    Text("\(self.duration)").font(.system(size: 56)).foregroundColor(.white)
-                        .onReceive(timer) { time in
-                            if self.duration > 0 {
-                                self.duration -= 1
-                            }
+            // Circle and Timer
+            ZStack {
+                Circle().fill(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .bottomLeading, endPoint: .topTrailing)).frame(width: 200, height: 200)
+                Text("\(self.duration)").font(.system(size: 64)).fontWeight(.bold).foregroundColor(.white)
+                    .onReceive(timer) { time in
+                        if self.duration > 0 {
+                            self.duration -= 1
                         }
-                        .onDisappear() {
-                            self.timer.upstream.connect().cancel()
-                        }
-                }
-                    .onAppear() {
-                        self.fill = 1.0
                     }
+                    .onDisappear() {
+                        self.timer.upstream.connect().cancel()
+                    }
+            }.onAppear() {
+                self.fill = 1.0
+            }
+            
+            // Stop, pause/resume, and repeat buttons
+            // TODO: make buttons images of stop, pause/play, and repeat arrow
+            VStack {
                 Spacer()
+                HStack {
+                    Button(action: {
+                        print("stop")
+                    }) {
+                        Text("stop")
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.white)
+                            .background(.gray)
+                            .clipShape(Circle())
+                    }.padding()
+                    Button(action: {
+                        print("pause/resume")
+                    }) {
+                        Text("pause/resume")
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.white)
+                            .background(.gray)
+                            .clipShape(Circle())
+                    }.padding()
+                    Button(action: {
+                        print("restart")
+                    }) {
+                        Text("restart")
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.white)
+                            .background(.gray)
+                            .clipShape(Circle())
+                    }.padding()
+                }.padding(.bottom, 25)
+            }
         }
     }
 }
